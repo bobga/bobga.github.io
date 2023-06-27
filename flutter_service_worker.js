4af0cc6269009e1080935e3c656353caf0f74956
Hunter Moore <59568398+bobga@users.noreply.github.com>
2023-06-27 21:42:45 +0300

    added tabbar navigator and fixed timer issue

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2eb299073b8c45ca7836d9ce7a659e4b",
-"/": "2eb299073b8c45ca7836d9ce7a659e4b",
-"main.dart.js": "a0812f5550a2f1cbf2dbd1a1041397cc",
+"index.html": "fe00d04278fccf274f0a09a0a54f1c83",
+"/": "fe00d04278fccf274f0a09a0a54f1c83",
+"main.dart.js": "fe39c84077417bc427de48589f102e30",
 "manifest.json": "f5a7afa0121e0598d264788dd5b892a0",
 "version.json": "3bb9d7d947c71f2e12880d815e0b7a4c"
 };
