4f6fb47f77f9852cc97b342dc494df843d499fce
Hunter Moore <59568398+bobga@users.noreply.github.com>
2023-06-27 20:35:15 +0300

    added a small progress indicator next to callsign

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "17ce1b213fe9a0152f026a21d3dcc69c",
-"/": "17ce1b213fe9a0152f026a21d3dcc69c",
-"main.dart.js": "1f33bb3dc7669022c549ba76575348f7",
+"index.html": "33072878935568188f4245f8c62e5cd1",
+"/": "33072878935568188f4245f8c62e5cd1",
+"main.dart.js": "02d618315414489d0099aa6f4ea48e35",
 "manifest.json": "f5a7afa0121e0598d264788dd5b892a0",
 "version.json": "3bb9d7d947c71f2e12880d815e0b7a4c"
 };
